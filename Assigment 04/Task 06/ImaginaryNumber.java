de867d37377b753bf05cc9b3955885b278a04036
Mazid Zomader <159147937+Mazid-Zomader@users.noreply.github.com>
2024-08-13 22:57:19 +0600

    Add files via upload

---
+++ Assigment 04/Task 06/ImaginaryNumber.java
@@ -0,0 +1,7 @@
+public class ImaginaryNumber {
+    public int realPart = 0;
+    public int imaginaryPart = 0;
+    public void printNumber(){
+        System.out.println(realPart + " + " + imaginaryPart+"i ");
+    }
+}
