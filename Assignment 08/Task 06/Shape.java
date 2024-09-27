86ad106a448a2e9792d24ee002bcd694cca94d13
Mazid Zomader <159147937+Mazid-Zomader@users.noreply.github.com>
2024-09-27 23:45:14 +0600

    Create Shape.java

---
+++ Assignment 08/Task 06/Shape.java
@@ -0,0 +1,8 @@
+public class Shape {
+    public String name;
+    public String color;
+
+    public void displayInfo() {
+        System.out.printf("Name: %s\nColor: %s\n", name, color);
+    }
+}
