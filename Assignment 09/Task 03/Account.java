18097aac8f4670df4abf9cc16a150f1d62ba128f
Mazid Zomader <159147937+Mazid-Zomader@users.noreply.github.com>
2024-09-28 11:40:49 +0600

    Create Account.java

---
+++ Assignment 09/Task 03/Account.java
@@ -0,0 +1,10 @@
+public class Account{
+    public double balance = 0.0;
+
+    public Account(double balance){
+        this.balance = balance;
+    }
+    public double showBalance(){
+        return balance;
+    }
+}
